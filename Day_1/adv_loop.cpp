a78cf0309d6f319964e3dec526f337e7704037b1
Chaitali Jadhav <88224453+ChaitaliJadhav14@users.noreply.github.com>
2025-11-10 22:06:06 +0530

    Add files via upload

---
+++ Day_1/adv_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+ for(int i=1,j=9;i<=9 && j>=1 ;i++,j--)	
+  	{
+  	 cout<<endl<<i<<"+"<<j<<"="<<(i+j);
+  }
+/* 	 
+	  
+ for(int i=1;i<=9;i++)
+ {
+ 	 cout<<endl<<i<<"+"<<(10-i)<<"="<<(i+(10-i));
+ }
+  }*/
+}
